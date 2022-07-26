bf3a82b7bc2636e1884cb410bf5e95acfb1a59d5
ho02 clouddnagmbhgmail.onmicrosoft.com <ho02@clouddnagmbhgmail.onmicrosoft.com>
2022-07-26 13:57:30 +0000

    odata

--- webapp/controller/Customer.controller.js
+++ webapp/controller/Customer.controller.js
@@ -1,14 +1,35 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, MessageBox) {
         "use strict";
 
         return Controller.extend("sct.training.jhoui5.controller.Main", {
             onInit: function () {
             },
+            onDelete: function (oEvent) {
+                this._onDelete(oEvent.getParameters().listItem)
+            },
+            _onDelete: function (oListItem) {
+                const oModel = this.getView().getModel();
+                const oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+                // const oSource = oEvent.getSource();
+                const sPath = oListItem.getBindingContext().getPath();
+
+                MessageBox.warning(oResourceBundle.getText("deleteWarning"), {
+                    title: oResourceBundle.getText("deleteWarningTitle"),
+                    actions: [MessageBox.Action.YES, MessageBox.Action.NO],
+                    emphasizedAction: MessageBox.Action.YES,
+                    onClose: function (oAction) {
+                        if(MessageBox.Action.YES == oAction) {
+                            oModel.remove(sPath);                            
+                        }
+                    }
+                })
+            }
         });
     });
